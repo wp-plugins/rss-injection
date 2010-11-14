0062dac0ec9e989b740019a5858888276fe06ffd
dcoda <dcoda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-14 11:16:42 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rss-injection/tags/1.2.1@311244 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ application/Controllers/RSSInjectionFiltersController.php
@@ -0,0 +1,64 @@
+<?php
+class RSSInjectionFiltersController extends w3v_Controller_Action_Filter
+{
+	public function the_contentAction ( $content )
+	{
+		if (get_post_type() == 'post')
+		{
+			$dataObj = new RSSInjectionData ( );
+			$data = $dataObj->get ();
+			$header = $this->dcode ( stripslashes($data ['header']) );
+			$footer = $this->dcode ( stripslashes($data ['footer']) );
+			$content = $header . $content . $footer;
+		}
+		return $content;
+	}
+
+	private function dcode ( $string )
+	{
+		$a = explode ( '[else]' , $string );
+		$rss = '';
+		$post = '';
+		if (count ( $a ) > 0)
+		{
+			$rss = $a [0];
+		}
+		if (count ( $a ) > 1)
+		{
+			$post = $a [1];
+		}
+		if (is_feed ())
+		{
+			$return = $rss;
+		}
+		else
+		{
+			$return = $post;
+		}
+		$replace = array ( 
+			'post_author' => 'get_the_author' , 'post_url' => 'get_permalink' , 'post_date' => 'get_the_modified_date' , 'post_time' => 'get_the_modified_time' , 'post_title' => 'get_the_title' , 'blog_url' => array ( 
+			'get_bloginfo' , array ( 
+			'url' 
+		) 
+		) , 'blog_name' => array ( 
+			'get_bloginfo' , array ( 
+			'name' 
+		) 
+		) 
+		);
+		foreach ( array_keys ( $replace ) as $key )
+		{
+			if (is_array ( $replace [$key] ))
+			{
+				$value = call_user_func_array ( $replace [$key] [0] , $replace [$key] [1] );
+			}
+			else
+			{
+				$value = call_user_func ( $replace [$key] );
+			}
+			$return = str_replace ( "[$key]" , $value , $return );
+		}
+		return $return;
+	}
+}
+		
---
+++ rss-injection.php
@@ -0,0 +1,17 @@
+<?php /*
+Plugin Name: RSSInjection
+Plugin URI: http://wordpress.org/extend/plugins/rss-injection/
+Description: Inject content into your RSS feed to entice people to subscribe or allow you to add a message so if the feed it aggregated onto another site it is at least attribute.
+Author: dcoda
+Author URI: http://dcoda.co.uk
+Version: 1.2.1
+ */ 
+$lib = dirname ( __FILE__ ) . '/library/wordpress/w3v/Application.php';
+if (! file_exists ( $lib )) {
+	require_once dirname ( __FILE__ ) . '/' . basename ( __FILE__, '.php' ) . '/' . basename ( __FILE__ );
+} else {
+	require_once $lib;
+
+
+	new w3v_Application ( __FILE__,array('RSSInjectionData') );
+}
