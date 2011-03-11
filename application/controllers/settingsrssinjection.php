16491d4c4cec2ddb97a0cffdd12d9640803e46ec
dcoda <dcoda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-11 11:10:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rss-injection/trunk@358738 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- application/controllers/settingsrssinjection.php
+++ application/controllers/settingsrssinjection.php
@@ -1,6 +1,6 @@
 <?php
 class settingsrssinjection extends wv23v_controller_action_adminmenu {
-	public function SettingsAction($content)
+	public function settingsAction($content)
 	{
 		$this->view->data = $this->settings()->post('options');
 		return $content . $this->updated();
