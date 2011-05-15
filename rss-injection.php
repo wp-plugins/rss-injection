5ab4e15511981ad5312f3bff73e009681b914dea
dcoda <dcoda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-15 17:26:26 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rss-injection/trunk@385148 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rss-injection.php
+++ rss-injection.php
@@ -4,7 +4,7 @@ Plugin URI: http://dcoda.co.uk/wordpress/
 Description: Inject content into your RSS feed to entice people to subscribe or allow you to add a message so if the feed it aggregated onto another site it is at least attribute.
 Author: dcoda
 Author URI: http://dcoda.co.uk
-Version: 1.5.0.28
+Version: 1.5.28
  */ 
 require_once  dirname ( __FILE__ ) . '/library/wordpress/application.php';
 $rssinjection = new wv28v_application ( __FILE__,array(),'rssinjectionsettings' );
