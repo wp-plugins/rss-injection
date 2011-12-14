c433cf5800472370cc66e13c582a0339c608cdee
dcoda <dcoda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-14 12:48:58 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rss-injection/tags/3.2.43@475373 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php /*
+Plugin Name: RSSInjection
+Plugin URI: http://redactor.dcoda.co.uk/donate/
+Description: Inject content into your RSS feed to entice people to subscribe or allow you to add a message so if the feed it aggregated onto another site it is at least attribute.
+Author: dcoda
+Author URI: 
+Version: 3.2.43
+License: GPLv2 or later
+*/
+@require_once  dirname ( __FILE__ ) . '/library/wordpress/application.php';
+if (class_exists("wv43v_application"))
+{
+	new wv43v_application ( __FILE__);
+}
