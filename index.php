72cd9733dc0b4ef9f887a10a938c89030abd56b3
dcoda <dcoda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-21 14:22:46 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rss-injection/trunk@478568 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ Version: 3.2.43
 License: GPLv2 or later
 */
 @require_once  dirname ( __FILE__ ) . '/library/wordpress/application.php';
-if (class_exists("wv43v_application"))
+if (class_exists("wv45v_application"))
 {
-	new wv43v_application ( __FILE__);
+	new wv45v_application ( __FILE__);
 }
